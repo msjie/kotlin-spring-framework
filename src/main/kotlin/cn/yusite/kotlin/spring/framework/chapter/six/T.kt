8aaf9da001230c7d6693a01906f597d59a37b488
sjie <ligongdamao@163.com>
2020-04-27 13:08:43 +0800

    parent factory

--- src/main/kotlin/cn/yusite/kotlin/spring/framework/chapter/six/T.kt
+++ src/main/kotlin/cn/yusite/kotlin/spring/framework/chapter/six/T.kt
@@ -1,11 +1,21 @@
 package cn.yusite.kotlin.spring.framework.chapter.six
 
+import org.apache.commons.dbcp2.BasicDataSource
+import java.sql.Connection
+
 class T private constructor(x: String) {
 
     val x: String = x
 
+    var basicDataSource: BasicDataSource? = null
+
     fun hi(): Unit {
 
         println(x)
+    }
+
+    fun getConnection(): Connection? {
+
+        return basicDataSource?.connection
     }
 }
--- src/test/kotlin/cn/yusite/kotlin/spring/framework/chapter/six/PlaceHolderConfigurerTest.kt
+++ src/test/kotlin/cn/yusite/kotlin/spring/framework/chapter/six/PlaceHolderConfigurerTest.kt
@@ -4,18 +4,27 @@ import cn.yusite.kotlin.spring.framework.chapter.four.Y
 import kotlin.test.Test
 
 import org.springframework.beans.factory.getBean
+import org.springframework.context.ApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
 
 class PlaceHolderConfigurerTest {
 
     /**
-     * 怎么解决spring el表单时，使用指定文件
+     * 1：父子容器
+     *
+     * 2：使用属性占位符
      */
     @Test
     fun `place holder configurer test`(): Unit {
 
-        val beanFactory = ClassPathXmlApplicationContext("chapter6/beans.xml")
-        val bean = beanFactory.getBean<T>("t")
-        bean.hi()
+        val parentBeanFactory = ClassPathXmlApplicationContext("chapter5/beans.xml")
+        if (parentBeanFactory is ApplicationContext) {
+            val beanFactory = ClassPathXmlApplicationContext(arrayOf("chapter6/beans.xml"), true, parentBeanFactory)
+            beanFactory.parent = parentBeanFactory
+            val bean = beanFactory.getBean<T>("t")
+            bean.hi()
+            println(bean.getConnection())
+        }
+
     }
 }
